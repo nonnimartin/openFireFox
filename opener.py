57e8e345abd2bafff2afbb500c8678f5723ec8cf
nonnimartin <nonnimartin@gmail.com>
2019-01-23 00:44:05 -0800

    added soup and other methods

--- opener.py
+++ opener.py
@@ -3,6 +3,11 @@ import os
 import subprocess
 import time
 import json
+import random
+from bs4 import BeautifulSoup
+import requests
+
+border = "======================================================="
 
 def openFireFox(target):
 	p = subprocess.Popen(["firefox", target])
@@ -13,12 +18,46 @@ def getTarget():
 		data = json.load(f)
 	return data['target']
 
+def getMinutes():
+	with open('config.json') as f:
+		data = json.load(f)
+	return int(data['min'])
+
+def getRandomNumber(top):
+	for x in range(10):
+		return random.randint(1,top)
+
+def getBsObject(html):
+	soup = BeautifulSoup(html, 'html.parser')
+	return soup
+
+def getHtml(target):
+	r = requests.get(target)
+	return r.content
+
 def main():
-	target = getTarget()
-	#open firefox tab
-	firefox = openFireFox(target)
-	#time.sleep(10)
-	#firefox.kill()
+
+	target  = getTarget()
+	minutes = getMinutes()
+	seconds = minutes * 100
+
+	html    = getHtml(target)
+	soup    = getBsObject(html)
+	hrefs soup.find_all('a')
+
+	
+	# while True:
+		
+	# 	#open firefox tab
+	# 	firefox = openFireFox(target)
+	# 	print border
+	# 	print "Opening Firefox for " + str(minutes) + " minutes"
+	# 	print " "
+	# 	time.sleep(seconds)
+	# 	firefox.kill()
+	# 	print "Killed Firefox"
+	# 	print " "
+	# 	print border
 
 
 if __name__ == '__main__':
