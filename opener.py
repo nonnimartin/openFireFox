c9d87a36becc02967e0d8ed84ba5e6546ee16e21
nonnimartin <nonnimartin@gmail.com>
2019-01-22 23:39:22 -0800

    added config file

--- opener.py
+++ opener.py
@@ -2,14 +2,21 @@ import webbrowser as wb
 import os
 import subprocess
 import time
+import json
 
-def openFireFox():
-	p = subprocess.Popen(["firefox", "http://www.google.com"])
+def openFireFox(target):
+	p = subprocess.Popen(["firefox", target])
 	return p
 
+def getTarget():
+	with open('config.json') as f:
+		data = json.load(f)
+	return data['target']
+
 def main():
+	target = getTarget()
 	#open firefox tab
-	firefox = openFireFox()
+	firefox = openFireFox(target)
 	#time.sleep(10)
 	#firefox.kill()
 
